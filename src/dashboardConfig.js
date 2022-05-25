cfb97aad55ad39ee17c3c5a141c57280e008874c
Nicole Jesudason <nicole.jesudason@gmail.com>
2022-05-25 15:44:17 +1000

    update sanity dashboard

--- src/dashboardConfig.js
+++ src/dashboardConfig.js
@@ -6,6 +6,12 @@ export default {
         title: "My Netlify deploys",
         sites: [
           {
+            title: "Internics Studio",
+            apiId: "a2c74872-b1f6-4bb4-ad17-c1b31795f665",
+            buildHookId: "628dc10b6bc9ba34a3ebab2b",
+            name: "internics-studio",
+          },
+          {
             title: "Internics",
             apiId: "c6394f16-f8d8-4315-b418-955e2129ec4a",
             buildHookId: "628d78ed61832b08af4e6e0c",
