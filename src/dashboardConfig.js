d8c085dfd611596c2e4bde4d7ac626e0e69e91f8
Nicole Jesudason <nicole.jesudason@gmail.com>
2022-05-25 10:40:24 +1000

    add redirects and dashboard

---
+++ src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Internics",
+            apiId: "c6394f16-f8d8-4315-b418-955e2129ec4a",
+            buildHookId: "628d78ed61832b08af4e6e0c",
+            name: "internics",
+          },
+        ],
+      },
+    },
+  ],
+};
